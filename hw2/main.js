3bbb69da18c9c5c33399c666272ce18fe3b29a18
HotzKepler <jason501002@gmail.com>
2022-10-02 12:58:09 +0800

    basic done

--- hw2/main.js
+++ hw2/main.js
@@ -1,116 +1,137 @@
-var total_people = 6
-var main_people = 1
-
 window.addEventListener('load', function() {
-    const close1 = document.getElementById('btm-close1')
-    const close2 = document.getElementById('btm-close2')
-    const close3 = document.getElementById('btm-close3')
-    const close4 = document.getElementById('btm-close4')
-    const close5 = document.getElementById('btm-close5')
-    
-    const close_win1 = document.getElementById("window1")
-    const close_win2 = document.getElementById("window2")
-    const close_win3 = document.getElementById("window3")
-    const close_win4 = document.getElementById("window4")
-    const close_win5 = document.getElementById("window5")
-    
-    const main_window_size = document.getElementById("main-wins")
-    const sub_window_size = document.getElementById("sub-wins")
-    const main_window_style = document.getElementById("main-user")
-    const main_window_func = document.getElementById("more-func")
-    const main_window_mute = document.getElementById("main-mute")
 
 
-    const main_window_user = document.getElementById("main-user")
-    const main_window_name = document.getElementById("main-name")
-    const pin1 = document.getElementById("more-win-func1")
-    const pin2 = document.getElementById("more-win-func2")
-    const pin3 = document.getElementById("more-win-func3")
-    const pin4 = document.getElementById("more-win-func4")
-    const pin5 = document.getElementById("more-win-func5")
+    closes = document.getElementsByClassName("btm-close")
+    Array.prototype.forEach.call(closes, function(items){
+        items.addEventListener('click', function(){
+            items.parentElement.remove()
+            main_people = document.getElementById("main-wins").childElementCount
+            total_people = document.getElementsByClassName("win").length
 
-    close1.addEventListener('click', function(){
-    let htmlstr = "<!-- " + close_win1.outerHTML + " -->"
-    close_win1.outerHTML = htmlstr
-    total_people = total_people - 1
-    })
-    close2.addEventListener('click', function(){
-        let htmlstr = "<!-- " + close_win2.outerHTML + " -->"
-        close_win2.outerHTML = htmlstr
-        total_people = total_people - 1
-    })
-    close3.addEventListener('click', function(){
-        let htmlstr = "<!-- " + close_win3.outerHTML + " -->"
-        close_win3.outerHTML = htmlstr
-        total_people = total_people - 1
-    })
-    close4.addEventListener('click', function(){
-        let htmlstr = "<!-- " + close_win4.outerHTML + " -->"
-        close_win4.outerHTML = htmlstr
-        total_people = total_people - 1
-    })
-    close5.addEventListener('click', function(){
-        let htmlstr = "<!-- " + close_win5.outerHTML + " -->"
-        close_win5.outerHTML = htmlstr
-        total_people = total_people - 1
-    })
-    
-    
-    pin1.addEventListener('click', function(){
-        if(main_people === 1){
-            win_color = document.getElementById("win-user1")
-            temp_color = getComputedStyle(win_color, null).backgroundColor
-            win_color.style.backgroundColor = "" + getComputedStyle(main_window_user, null).backgroundColor
-            main_window_user.style.backgroundColor = temp_color
-            win_name = document.getElementById("win-user1")
-        }
-    })
-    pin2.addEventListener('click', function(){
-        if(main_people === 1){
-            win_color = document.getElementById("win-user2")
-            temp_color = getComputedStyle(win_color, null).backgroundColor
-            win_color.style.backgroundColor = "" + getComputedStyle(main_window_user, null).backgroundColor
-            main_window_user.style.backgroundColor = temp_color
-        }
-    })
-    pin3.addEventListener('click', function(){
-        if(main_people === 1){
-            win_color = document.getElementById("win-user3")
-            temp_color = getComputedStyle(win_color, null).backgroundColor
-            win_color.style.backgroundColor = "" + getComputedStyle(main_window_user, null).backgroundColor
-            main_window_user.style.backgroundColor = temp_color
-        }
-    })
-    pin4.addEventListener('click', function(){
-        if(main_people === 1){
-            win_color = document.getElementById("win-user4")
-            temp_color = getComputedStyle(win_color, null).backgroundColor
-            win_color.style.backgroundColor = "" + getComputedStyle(main_window_user, null).backgroundColor
-            main_window_user.style.backgroundColor = temp_color
-        }
-    })
-    pin5.addEventListener('click', function(){
-        if(main_people === 1){
-            win_color = document.getElementById("win-user5")
-            temp_color = getComputedStyle(win_color, null).backgroundColor
-            win_color.style.backgroundColor = "" + getComputedStyle(main_window_user, null).backgroundColor
-            main_window_user.style.backgroundColor = temp_color
-        }
+
+            if(total_people === 1 && main_people === 1) {
+                main = document.getElementById("main-wins")
+                sub = document.getElementById("sub-wins")
+                main.style = `
+                    width: 95%;
+                `
+                sub.style.display = "none"
+            }
+            if(total_people === 1 && main_people === 0){
+                //console.log("test")
+                temp_main_win = document.getElementById("main-wins")
+                temp_sub_user = document.getElementById("window0")
+                
+                temp_main_win.appendChild(temp_sub_user)
+                // console.log(temp_main_win, temp_sub_user)
+                main = document.getElementById("main-wins")
+                sub = document.getElementById("sub-wins")
+                main.style = `
+                    height: 85%;
+                    width: 95%;
+                `
+                main.children[0].style = `
+                    height: 95%;
+                    width: 95%;
+                `
+                sub.style.display = "none"
+            }
+        })
     })
 
 
 
 
-    this.window.addEventListener('mouseover', function() {
-        if(total_people === 1) {
-            main_window_size.style = `width: 90%;
-            left: 10%;
-            `
-            main_window_mute.style = "left: 75%;"
-            sub_window_size.style = "width: 0%"
-            main_window_style.style = "width: 14%"
-            main_window_func.style = "width: 10%"
-        }
+
+
+
+    pin = document.getElementsByClassName("win-func")
+    Array.prototype.forEach.call(pin, function(items){
+
+        items.addEventListener('click', function(){
+            main_people = document.getElementById("main-wins").childElementCount
+            total_people = document.getElementsByClassName("win").length
+
+            if(items === pin[0] && main_people === 1){
+                temp_main_win = document.getElementById("main-wins")
+                temp_main_user = temp_main_win.children[0]
+                temp_sub_win = pin[1].parentElement.parentElement
+                // temp_sub_win = pin[1].parentElement.parentElement
+
+                temp_main_win.children[0].remove()
+                temp_sub_win.appendChild(temp_main_user)
+                // console.log("temp_sub_win")
+                // console.log(temp_sub_win)
+                
+                // console.log(getComputedStyle(temp_sub_win, null).width)
+                temp_sub_win.style.width = "90%"
+                    
+                temp_main_win.style =`
+                    width: 0%;
+                `
+                Array.prototype.forEach.call(pin, function(win_items) {
+                    win_items.parentElement.style = `
+                        height: 40%;
+                        width: 30%;
+                    `
+                    // console.log(items);
+                });
+            }
+            else if(items !== pin[0] && main_people === 1){
+                temp_main_win = document.getElementById("main-wins")
+                temp_main_user = temp_main_win.children[0]
+                temp_sub_user = items.parentElement
+                temp_sub_win = items.parentElement.parentElement
+                // console.log("temp_main_user")
+                // console.log(temp_main_user)
+                // temp_main_win.children[0].remove()
+
+                temp_main_user.style = `
+                    height: 30%;
+                    width: 45%;
+                `
+                temp_sub_user.style = `
+                    height: 95%;
+                    width: 95%;
+                `
+                temp_main_win.removeChild(temp_main_user)               
+                items.parentElement.remove()
+                temp_main_win.appendChild(temp_sub_user)
+                temp_sub_win.appendChild(temp_main_user)
+                
+            }
+            else if(main_people === 0){
+                //console.log("tests1")
+                temp_main_win = document.getElementById("main-wins")
+                temp_sub_win = items.parentElement.parentElement
+                temp_sub_user = items.parentElement
+                // console.log("temp_sub_user")
+                // console.log(temp_sub_user)
+                temp_sub_win.removeChild(temp_sub_user)
+                temp_main_win.appendChild(temp_sub_user)
+                // // items.parentElement.remove()
+                
+                main_people = 1
+                
+                temp_sub_win.style.width = "30%"  
+                temp_main_win.style =`
+                    width: 65%;
+                `
+                fix = document.getElementsByClassName("win")
+                // console.log(fix[0])
+                for(i = 0; i < fix.length; i++) {
+                    fix[i].style = `
+                        height: 30%;
+                        width: 45%;
+                    `
+                }
+                main = document.getElementById("main-wins")
+                main.children[0].style = `
+                    height: 95%;
+                    width: 95%;
+                `
+            }
+        })
     })
+
 })
-
