d610376e28cc9f621614143713751b73adcfe5c3
HotzKepler <jason501002@gmail.com>
2022-10-16 10:23:41 +0800

    basic function done

---
+++ hw4/src/util/randomFixSeed.js
@@ -0,0 +1,21 @@
+/****************************************************************************
+  FileName      [ randomFixSeed.js ]
+  PackageName   [ src/util ]
+  Author        [ Cheng-Hua Lu ]
+  Synopsis      [ This file makes random numbers. ]
+  Copyright     [ 2022 10 ]
+****************************************************************************/
+
+
+
+// var seedrandom = require('seedrandom');
+// const randomFixSeed = seedrandom('Over my dead body');
+
+// export default function randomNum(min = 0, max) {
+//   return Math.floor(randomFixSeed() * (max - min + 1) + min);
+// }
+
+
+export default function randomNum(min = 0, max) {
+  return Math.floor(1 * (max - min + 1) + min);
+}
