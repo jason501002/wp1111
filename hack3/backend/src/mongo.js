9c0381876b83d6ef70718caa9d0dcfcc61e1af47
HotzKepler <jason501002@gmail.com>
2022-12-20 09:46:33 +0800

    hack3

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+mongoose.set("strictQuery", true);
+
+async function connect() {
+  // TODO 1 Connect to your MongoDB and call dataInit()
+
+  // dotenv.config();
+  // mongoose.connect(process.env.MONGO_URL, {
+  //       useNewUrlParser: true,
+  //       useUnifiedTopology: true,
+  //   }).then((res) => console.log("mongo db connection created"));
+  mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+  }).then(async res => {
+        dataInit()
+  })
+
+  // TODO 1 End
+}
+
+export default { connect };
